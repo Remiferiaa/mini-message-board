ebe0465410075ca0d2020d3c77481a74c4907d77
Remiferiaa <slong819@hotmail.com>
2022-07-23 00:35:36 +0800

    general project finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Test1",
+    added: new Date().toLocaleString()
+  },
+  {
+    text: "Hello World!",
+    user: "Test2",
+    added: new Date().toLocaleString()
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages:messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: "Add your message"})
+})
+
+router.post('/new', function(req, res, next){
+  messages.push({
+    text:req.body.descrip,
+    user:req.body.author,
+    added: new Date().toLocaleString()
+  })
+  res.redirect('/')
+})
+
 
 module.exports = router;
